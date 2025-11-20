a35a21acd87133444b3f9902c1943d010b08ae4a
Markusgerholm <markus.gerholm1@gmail.com>
2025-11-20 11:59:21 +0100

    Torsdag förmiddag

---
+++ CE3/Data/4room.r
@@ -0,0 +1,54 @@
+library(ctsmr)
+library(splines)
+source("CE3/Data/sde4room_base.r")
+load("CE3/Data/Exercise3.RData")
+fit1 <- sde4room_base(AllDat)
+
+summary(fit1)
+summary(fit1, extended=TRUE)
+
+Pred1 <- predict(fit1)
+res1 <- Pred1[[1]]$state$pred$Ti1 - AllDat$yTi1
+res2 <- Pred1[[1]]$state$pred$Ti2 - AllDat$yTi2
+res3 <- Pred1[[1]]$state$pred$Ti3 - AllDat$yTi3
+res4 <- Pred1[[1]]$state$pred$Ti4 - AllDat$yTi4
+AllDat$res1 <- res1
+AllDat$res2 <- res2
+AllDat$res3 <- res3
+AllDat$res4 <- res4
+Hour <- as.numeric(strftime(AllDat$date, format="%H"))
+
+
+plot(Hour,AllDat$res1, 
+     col  = "blue",
+     xlab = "Hour",
+     ylab = "Residual",
+     main = "Residuals over time")
+
+points(Hour, res2,
+      col  = "red")
+
+points(Hour, res3,
+      col  = "green")
+
+points(Hour, res4,
+      col  = "orange")
+
+#legend("topright",
+#       legend = c("Ti1", "Ti2"),
+#       col    = c("blue", "red"),
+#       lty    = 1,
+#       bty    = "n")
+
+#rmse1 <- sqrt(mean((Pred[[1]]$state$pred$Ti - AllDat$yTi4)^2))
+rmse1 <- sqrt(mean(res1^2, na.rm = TRUE))
+rmse2 <- sqrt(mean(res2^2, na.rm = TRUE))
+rmse3 <- sqrt(mean(res3^2, na.rm = TRUE))
+rmse4 <- sqrt(mean(res4^2, na.rm = TRUE))
+
+rmse1
+rmse2
+rmse3
+rmse4
+
+acf(res1, lag.max = 30)
--- CE3/ExampleRoom1.r
+++ CE3/ExampleRoom1.r
@@ -2,14 +2,15 @@ library(ctsmr)
 library(splines)
 source("CE3/Data/sdeTiTm.R")
 load("CE3/Data/Exercise3.RData")
-fit1 <- sdeTiTm(AllDat,AllDat$yTi1,AllDat$Ph1)
+fit1 <- sdeTiTm(AllDat,AllDat$yTi4,AllDat$Ph2)
 
 summary(fit1,extended=TRUE)
 
 Hour <- as.numeric(strftime(AllDat$date, format="%H"))
 
 Pred <- predict(fit1)
-plot(Pred[[1]]$state$pred$Ti - AllDat$yTi1 ~ Hour)
+plot(Pred[[1]]$state$pred$Ti - AllDat$yTi4 ~ Hour)
+rmse1 <- sqrt(mean((Pred[[1]]$state$pred$Ti - AllDat$yTi4)^2))
 # What is going on 10 AM?
 # Try to fir a varying effective window area
 
@@ -43,8 +44,51 @@ AllDat$bs5 = bs5
 
 
 ### You will have to implement sdeTITmAv ###
-# source("sdeTiTmAv.R")
-#fit2 <- sdeTiTmAv(AllDat,AllDat$yTi1,AllDat$Ph1)
+source("CE3/data/sdeTiTm_spline.r")
+fit2 <- sdeTiTm_spline(AllDat,AllDat$yTi4,AllDat$Ph2)
 
 plot(bs[14:27,1]*fit2$xm[3]+bs[14:27,2]*fit2$xm[4]+bs[14:27,3]*fit2$xm[5]+bs[14:27,4]*fit2$xm[6]+bs[14:27,5]*fit2$xm[7],type='l')
+
+summary(fit2, extended = TRUEsumma)
+
+Hour <- as.numeric(strftime(AllDat$date, format="%H"))
+
+Pred2 <- predict(fit2)
+plot(Pred2[[1]]$state$pred$Ti - AllDat$yTi4 ~ Hour)
+rmse2 <- sqrt(mean((Pred2[[1]]$state$pred$Ti - AllDat$yTi4)^2))
+
+### You will have to implement sdeTITmAv ###
+source("CE3/data/sdeTiTm_heatfrac.r")
+fit3 <- sdeTiTm_heatfrac(AllDat,AllDat$yTi4,AllDat$Ph2)
+summary(fit3, extended = TRUE)
+Pred3 <- predict(fit3)
+plot(Pred3[[1]]$state$pred$Ti - AllDat$yTi4 ~ Hour)
+rmse3 <- sqrt(mean((Pred3[[1]]$state$pred$Ti - AllDat$yTi4)^2))
+
+source("CE3/data/sdteTiTm_T3T4.r") # Removed a2 spline basis as well
+fit4 <- sdeTiTm_T3T4(AllDat,AllDat$yTi4,AllDat$Ph2)
+summary(fit4, extended = TRUE)
+Pred4 <- predict(fit4)
+plot(Pred4[[1]]$state$pred$Ti - AllDat$yTi4 ~ Hour)
+rmse4 <- sqrt(mean((Pred4[[1]]$state$pred$Ti - AllDat$yTi4)^2))
+
+plot(bs[14:27,1]*fit2$xm[3]+bs[14:27,3]*fit2$xm[5]+bs[14:27,4]*fit2$xm[6]+bs[14:27,5]*fit2$xm[7],type='l')
+
+
+
+# RMSE
+print('RMSE:')
+print(rmse1)
+print('RMSE with splines:')
+print(rmse2)
+print('RMSE with splines & fractional heating:')
+print(rmse3)
+print('RMSE with splines & T3:')
+print(rmse4)
+
+# Plotting data
+plot(AllDat$Ph2 ~ Hour)
+
+
+
 
